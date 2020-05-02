48a73a2d188f14701779a073ba0c0c7ece8acdca
OscarFlores-IFi <if715029@gmail.com>
2020-05-01 19:06:17 -0500

    no funciona import

--- prueba.py
+++ prueba.py
@@ -1,37 +1,42 @@
+"""
+Created on Fri May  1 18:38:03 2020
+
+@author: of
+"""
+
 # Se simularan n escenarios, en los cuales se apuesta un 'all-in' para encontrar distribuciÃ³n adecuada para apostar el Tempest Hold'em.
 # Falta incorporar imports relativos y sacar el documento de la carpeta. 
 # https://realpython.com/absolute-vs-relative-python-imports/
-
 
 from deck import Deck
 from card import Card
 from evaluator import Evaluator
 import init_rank
 
-
-
-deck = Deck()
 evaluator = Evaluator()
 
-
 # Repartimos 2 cartas a cada jugador
-nplayers = 9
+
 def juego(nplayers, pretty_print = False):
+    deck = Deck()
     hands = [deck.draw(2) for _ in range(nplayers)]
     
     # Cada jugador decide si apuesta o no apuesta en base a su posición y su mano inicial.
-    init_rank = [initial_ranking(hand) for hand in hands] # ranking inicial p/2 jugadores, según Michael Shackleford. 
+    initial_rank = [init_rank.init_rank(hand) for hand in hands] # ranking inicial p/2 jugadores, según Michael Shackleford. 
     
-    # Se muestran las 5 cartas del piso. 
+    # Se reparte las 5 cartas comunes. 
     board = deck.draw(5)
     
     
     if pretty_print:
-        [Card.print_pretty_cards(i) for i in hands]
-        Card.print_pretty_cards(board)
-        evaluator.hand_summary(board, hands)
+        [Card.print_pretty_cards(i) for i in hands] # imprime la mano de cada jugador
+        Card.print_pretty_cards(board) # imprime tablero
+        evaluator.hand_summary(board, hands) # imprime ganador
     
-    
-    
-    # Se calcula quien fue el ganador. 
-    
+    # Se calcula el ranking de cada mano. 
+    ranking = [evaluator.evaluate(hand) for hand in hands]
+    return([initial_rank, ranking])
+    #
+
+nplayers = 9
+juego(nplayers)
